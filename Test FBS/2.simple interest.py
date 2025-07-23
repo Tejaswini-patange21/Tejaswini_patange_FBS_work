ed620cac7af92d73882a26fbf565e8b55f63882a
Tejaswini patange <patangeteju2004@gmail.com>
2025-07-23 21:55:02 +0530

    Add files via upload

---
+++ Test FBS/2.simple interest.py
@@ -0,0 +1,7 @@
+p= int(input("enter the principle amount:"))
+r= int(input("enter the rate:"))
+t= int(input("enter the time:"))
+
+simple_interest = p*r*t/100
+
+print("to find the simple interest:", simple_interest)
